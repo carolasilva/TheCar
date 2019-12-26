e691193de42a530e8fa04140f3ce4ca52c7f5d5c
Carolina Alves <carol.alves99@live.com>
2019-12-26 13:37:45 -0300

    project created

---
+++ src/test/java/com/thecar/thecar/ThecarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecar.thecar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThecarApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
